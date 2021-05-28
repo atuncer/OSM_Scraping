bce1f742fbc27087da7765117db8337d0ebb5aaa
Atahan Tuncer <44475212+atuncer@users.noreply.github.com>
2021-05-28 15:11:53 +0300

    renamed the database and edited value row to be integer

--- main.py
+++ main.py
@@ -1,32 +1,24 @@
-import time
-import pickle
+import time,pickle,sqlite3
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import sqlite3
 from sqlite3 import Error
 
-conn = sqlite3.connect('identifier.sqlite')
+conn = sqlite3.connect('players.db')
 
 opts = Options()
 opts.add_argument("start-maximized")
 opts.add_experimental_option("excludeSwitches", ['enable-automation'])
 opts.add_experimental_option('useAutomationExtension', False)
-#opts.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36')
 driver = webdriver.Chrome(executable_path='chromedriver.exe',options=opts)
 driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
 driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
 
 url = 'https://en.onlinesoccermanager.com/LeagueTypes/'
 
-
 driver.get(url)
 cookies = pickle.load(open("cookie.pkl", "rb"))
 for cookie in cookies:
     driver.add_cookie(cookie)
-
-
-# time.sleep(20)
-# pickle.dump( driver.get_cookies() , open("cookie.pkl","wb"))
 
 time.sleep(5)
 driver.get(url)
@@ -64,14 +56,17 @@ for i in range(lent):  # all leagues
                 plr.append(driver.find_element_by_xpath(f"/html/body/div[3]/div[4]/div/div/div/div[1]/div[2]/div/div/div/div/table/tbody[{l + 1}]/tr[{k + 1}]/td[8]").text)
                 plr.append(str(league).replace('\n',' '))
                 plr.append(str(team).replace('\n',' '))
-                plr.append(driver.find_element_by_xpath(f"/html/body/div[3]/div[4]/div/div/div/div[1]/div[2]/div/div/div/div/table/tbody[{l + 1}]/tr[{k + 1}]/td[11]/bdi/span/span[2]").text)
+                value = str(driver.find_element_by_xpath(f"/html/body/div[3]/div[4]/div/div/div/div[1]/div[2]/div/div/div/div/table//tbody[{l + 1}]/tr[{k + 1}]/td[11]/bdi/span/span[2]").text)
+                value = value.replace('K','000').replace('M','00000').replace('.','')
+                plr.append(value)
+
 
                 cmd = f"INSERT INTO players VALUES ('{plr[0]}','{plr[1]}',{plr[2]},'{plr[3]}',{plr[4]},{plr[5]},{plr[6]},'{plr[7]}','{plr[8]}','{plr[9]}')"
                 print(cmd,i,j)
                 try:
                     conn.cursor().execute(cmd)
                 except Error:
-                    print('error')
+                    print('This player is already saved')
                     conn.rollback()
             conn.commit()
 
